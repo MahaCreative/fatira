bc6b229a669d2fc58fe51b6626398fe369905834
MahaCreative <gunturmadjid.3@gmail.com>
2023-02-08 20:46:33 -0800

    asd

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,7 +22,7 @@ class ProfileController extends Controller
 
         $user = $request->user();
 
-        $url = $request->file('thumbnail') ? $request->file('thumbnail')->store('images/profile') : null;
+        $url = $request->file('thumbnail') ? $request->file('thumbnail')->store('public/images/profile') : null;
         // dd($request->all());
         $roles = $request->user()->roles()->first();
        if($roles->name === 'mahasiswa'){
@@ -67,7 +67,7 @@ class ProfileController extends Controller
     public function profileUpdate(Request $request){
 
         $user = $request->user()->profile;
-        $url = $request->file('thumbnail') ? $request->file('thumbnail')->store('images/profile') : $user->thumbnail;
+        $url = $request->file('thumbnail') ? $request->file('thumbnail')->store('public/images/profile') : $user->thumbnail;
         $user->update([
             'nama_lengkap' => $request->data['nama_lengkap'],
             'ttl' => $request->data['ttl'],
